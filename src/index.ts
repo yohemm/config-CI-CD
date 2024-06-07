4936b029bdc594d9b61d217195975affb45671ae
yohemm <vaxelaire.yohem@gmail.com>
2024-06-07 14:27:23 +0200

    test

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const sum = (a:number , b:number) => a+b
+const divide = (a:number , b:number) => a/b
+const multiplication = (a:number , b:number) => a*b
+const substract = (a:number , b:number) => a-b
+// const sum = (a:number , b:number) => a+b
